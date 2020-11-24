508150e2a9e726bfdd1566b002f0bd87b9777c55
SilasDerProfi <silas@schnurr.one>
2020-11-24 16:00:24 +0100

    Remove BoardPosition

--- spe-ed-solver/utility/src/main/java/utility/game/board/Board.java
+++ spe-ed-solver/utility/src/main/java/utility/game/board/Board.java
@@ -1,18 +1,32 @@
 package utility.game.board;
 
+import utility.geometry.Point2i;
+
 /**
  * Board
  */
 public class Board<CellType extends IBoardCell> {
 
-    private CellType[][] cells;
+    private final CellType[][] cells;
+    private final int height;
+    private final int width;
 
     public Board(CellType[][] cells) {
         this.cells = cells;
+        this.height = cells.length;
+        this.width = cells[0].length;
     }
 
-    public CellType getBoardCellAt(BoardPosition position) {
-        return cells[position.getY()][position.getX()];
+    public CellType getBoardCellAt(Point2i position) {
+        if (isOnBoard(position))
+            return cells[position.getY()][position.getX()];
+        else
+            return null;
+    }
+
+    public boolean isOnBoard(Point2i position) {
+        return 0 <= position.getX() && position.getX() < this.width && 0 <= position.getY()
+                && position.getY() < this.height;
     }
 
 }
