c5bc3fefdbb893e8de478bff42f7ece3aae07b96
Felix Lehnerer <felix.lehnerer@web.de>
2020-11-24 13:18:13 +0100

    Vector2i class for 2d integer vectors

---
+++ spe-ed-solver/utility/src/main/java/utility/geometry/Vector2i.java
@@ -0,0 +1,50 @@
+package utility.geometry;
+
+/**
+ * 2d vector with int values.
+ */
+public class Vector2i {
+
+	// size of the vector in each dimension
+	private final int x;
+	private final int y;
+
+	/**
+	 * Generates a new vector.
+	 * 
+	 * @param x
+	 * @param y
+	 */
+	public Vector2i(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	/**
+	 * Returns the size of the vector in the x dimension.
+	 * 
+	 * @return size of the vector in the x dimension
+	 */
+	public int getX() {
+		return x;
+	}
+
+	/**
+	 * Returns the size of the vector in the y dimension.
+	 * 
+	 * @return size of the vector in the y dimension
+	 */
+	public int getY() {
+		return y;
+	}
+	
+	/**
+	 * Multiplies the sizes of the vector in each dimension by the given factor
+	 * @param factor factor to multiply with
+	 * @return new vector as result
+	 */
+	public Vector2i multiply(int factor) {
+		return new Vector2i(x * factor, y * factor);
+	}
+
+}
