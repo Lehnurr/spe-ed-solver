566062c09b84c223a601b2578a1906c1c056b862
Felix Lehnerer <felix.lehnerer@web.de>
2020-11-24 12:45:18 +0100

    added viewer class to manage ViewerWindow

--- spe-ed-solver/visualisation/src/main/java/visualisation/Viewer.java
+++ spe-ed-solver/visualisation/src/main/java/visualisation/Viewer.java
@@ -2,10 +2,17 @@ package visualisation;
 
 public class Viewer {
 	
-	private final ViewerWindow window = new ViewerWindow();
+	private final ViewerWindow window;
+	
+	private int currentMaxRound = 0;
+	private int displayedRound = 0;
 	
 	
-	public Viewer() {
+	public Viewer(ViewerWindow viewerWindow) {
+		this.window = viewerWindow;
 	}
+	
+	// TODO addRound() -> update round counters
+	// TODO changeDisplayedRound()
 	
 }
