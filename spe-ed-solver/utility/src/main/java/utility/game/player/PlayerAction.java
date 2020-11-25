2bd801f59ed8d231ba3c7ead429810c41c4d89f6
Felix Lehnerer <felix.lehnerer@web.de>
2020-11-25 20:30:20 +0100

    added textual representation to PlayerAction

--- spe-ed-solver/utility/src/main/java/utility/game/player/PlayerAction.java
+++ spe-ed-solver/utility/src/main/java/utility/game/player/PlayerAction.java
@@ -1,10 +1,30 @@
 package utility.game.player;
 
+/**
+ * Enum to describe every possible action a player can do in a game round.
+ */
 public enum PlayerAction {
 
-	TURN_LEFT, TURN_RIGHT, SLOW_DOWN, SPEED_UP, CHANGE_NOTHING;
+	TURN_LEFT("TURN LEFT"), TURN_RIGHT("TURN RIGHT"), SLOW_DOWN("SLOW DOWN"), SPEED_UP("SPEED UP"),
+	CHANGE_NOTHING("CHANGE NOTHING");
 
-	private PlayerAction() {
+	// textual representation of the action type
+	private final String name;
+
+	private PlayerAction(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return name of the action type
+	 */
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public String toString() {
+		return getName();
 	}
 
 }
