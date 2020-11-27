f353ed8b4e02259a6a90ae07b881de8003f20dd7
SilasDerProfi <silas@schnurr.one>
2020-11-27 16:22:55 +0100

    Add CellValue Enum

---
+++ spe-ed-solver/utility/src/main/java/utility/game/board/CellValue.java
@@ -0,0 +1,22 @@
+package utility.game.board;
+
+/**
+ * A possible Cell-Value for the Game (PlayerId, 0 or -1)
+ */
+public enum CellValue {
+    MULTIPLE_PLAYER(-1), EMPTY_CELL(0), PLAYER_ONE(1), PLAYER_TWO(2), PLAYER_THREE(3), PLAYER_FOUR(4), PLAYER_FIVE(5),
+    PLAYER_SIX(6);
+
+    private final int value;
+
+    private CellValue(int value) {
+        this.value = value;
+    }
+
+    /**
+     * Returns the Cells value
+     */
+    public int getCellValue() {
+        return this.value;
+    }
+}
