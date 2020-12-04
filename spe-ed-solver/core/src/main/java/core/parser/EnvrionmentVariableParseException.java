d355dc61a28e78039af84b3f90961289c97a9804
Felix Lehnerer <felix.lehnerer@web.de>
2020-12-04 18:22:14 +0100

    added EnvironmentVariableParser for reading and parsing environment variables to specific data types

---
+++ spe-ed-solver/core/src/main/java/core/parser/EnvrionmentVariableParseException.java
@@ -0,0 +1,17 @@
+package core.parser;
+
+/**
+ * Exception thrown when environment variables cannot be used (e.g. invalid
+ * syntax).
+ */
+public class EnvrionmentVariableParseException extends Exception {
+
+	public EnvrionmentVariableParseException(String message) {
+		super(message);
+	}
+
+	public EnvrionmentVariableParseException(String message, Throwable throwable) {
+		super(message, throwable);
+	}
+
+}
