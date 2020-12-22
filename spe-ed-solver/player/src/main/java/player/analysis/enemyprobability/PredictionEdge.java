8147e337776041fa2617cb6bda7b09119f387641
Felix Lehnerer <felix.lehnerer@web.de>
2020-12-22 13:30:40 +0100

    progress on PredictionEdge and PredictionVertex

--- spe-ed-solver/player/src/main/java/player/analysis/enemyprobability/PredictionEdge.java
+++ spe-ed-solver/player/src/main/java/player/analysis/enemyprobability/PredictionEdge.java
@@ -1,8 +1,10 @@
 package player.analysis.enemyprobability;
 
+import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import utility.game.board.Board;
 import utility.game.board.Cell;
@@ -18,10 +20,11 @@ import utility.geometry.Vector2i;
  */
 public class PredictionEdge {
 
-	private final PredictionVertex startVertex;
 	private final PredictionVertex targetVertex;
 
 	private final PlayerAction action;
+
+	private final Collection<Point2i> points;
 
 	private final boolean valid;
 
@@ -36,49 +39,125 @@ public class PredictionEdge {
 	public PredictionEdge(final Board<Cell> board, final PredictionVertex startVertex, final PlayerAction action) {
 
 		this.action = action;
-		this.startVertex = startVertex;
 
-		int nextSpeed = startVertex.getSpeed();
+		final int targetSpeed = calculateTargetSpeed(startVertex.getSpeed(), action);
+		final PlayerDirection targetDirection = startVertex.getDirection().doAction(action);
+		final Point2i targetPosition = calculateTargetPoint(startVertex.getPosition(), targetDirection, targetSpeed);
+		final int targetRound = startVertex.getRound() + 1;
+
+		this.points = calculatePoints(targetDirection, startVertex.getPosition(), targetPosition, targetSpeed,
+				targetRound);
+
+		final Set<Point2i> targetRequiredPoints = new HashSet<>(startVertex.getRequiredPoints());
+		targetRequiredPoints.addAll(this.points);
+
+		this.targetVertex = new PredictionVertex(board, targetPosition, targetDirection, targetSpeed, targetRound,
+				targetRequiredPoints);
+
+		this.valid = calculateValid(board, targetSpeed, targetPosition, this.points, startVertex.getRequiredPoints());
+	}
+
+	/**
+	 * Calculates the target speed of the edge out of the base speed and a given
+	 * {@link PlayerAction}.
+	 * 
+	 * @param startSpeed speed before the action is performed
+	 * @param action     {@link PlayerAction} action to calculate with
+	 * @return speed after the action is performed
+	 */
+	private int calculateTargetSpeed(final int startSpeed, final PlayerAction action) {
+		int nextSpeed = startSpeed;
 		if (action == PlayerAction.SPEED_UP)
 			nextSpeed += 1;
 		else if (action == PlayerAction.SLOW_DOWN)
 			nextSpeed -= 1;
-
-		PlayerDirection nextDirection = startVertex.getDirection().doAction(action);
-
-		Vector2i movementVector = nextDirection.getDirectionVector().multiply(nextSpeed);
-		Point2i targetPosition = startVertex.getPosition().translate(movementVector);
-
-		if (nextSpeed > IPlayer.MAX_SPEED || nextSpeed < IPlayer.MIN_SPEED || board.isOnBoard(targetPosition)) {
-			this.valid = false;
-		} else {
-			this.valid = true;
-		}
-
-		final int nextRound = startVertex.getRound() + 1;
-
-		this.targetVertex = new PredictionVertex(board, targetPosition, nextDirection, nextSpeed, nextRound);
+		return nextSpeed;
 	}
 
 	/**
-	 * Returns all {@link Point2i Point2i}s of the {@link PredictionEdge}. If the
-	 * {@link PredictionEdge} is not valid, an empty collection is returned.
+	 * Calculates the target position of the edge with a given
+	 * {@link PlayerDirection} and the speed the player moves with.
 	 * 
-	 * @return {@link Point2i}s of the edge
+	 * @param startPoint {@link Point2i} of the edge start position
+	 * @param direction  {@link PlayerDirection} the player moves in
+	 * @param speed      speed the player moves with
+	 * @return {@link Point2i} of the edge end position
 	 */
-	public Collection<Point2i> getPoints() {
-		if (valid) {
-			final Vector2i directionVector = targetVertex.getDirection().getDirectionVector();
-			final Point2i firstPointInDirection = startVertex.getPosition().translate(directionVector);
-			final List<Point2i> points = firstPointInDirection.pointsInRectangle(targetVertex.getPosition());
-			if (targetVertex.getRound() % 6 == 0 && targetVertex.getSpeed() > 2) {
-				points.remove(points.size() - 1);
-				points.remove(0);
-			}
-			return points;
-		} else {
-			return Collections.emptySet();
+	private Point2i calculateTargetPoint(final Point2i startPoint, final PlayerDirection direction, final int speed) {
+		final Vector2i movementVector = direction.getDirectionVector().multiply(speed);
+		final Point2i targetPosition = startPoint.translate(movementVector);
+		return targetPosition;
+	}
+
+	/**
+	 * Calculates all points contained in the {@link PredictionEdge}. It is not
+	 * granted that all {@link Point2i points} are on the {@link Board}.
+	 * 
+	 * @param targetDirection {@link PlayerDirection} the direction after the edge
+	 * @param startPosition   {@link Point2i} of the edge start position
+	 * @param targetPosition  {@link Point2i} of the edge end position
+	 * @param targetSpeed     speed the player has after the edge
+	 * @param targetRound     round after the edge
+	 * @return {@link Collection} of contained {@link Point2i points}
+	 */
+	private Collection<Point2i> calculatePoints(final PlayerDirection targetDirection, final Point2i startPosition,
+			final Point2i targetPosition, final int targetSpeed, final int targetRound) {
+
+		final Vector2i directionVector = targetDirection.getDirectionVector();
+		final Point2i firstPointInDirection = startPosition.translate(directionVector);
+
+		final List<Point2i> points = firstPointInDirection.pointsInRectangle(targetPosition);
+		if (targetRound % 6 == 0 && targetSpeed > 2) {
+			return Arrays.asList(points.get(0), points.get(points.size() - 1));
 		}
+		return points;
+	}
+
+	/**
+	 * Checks if the {@link PredictionEdge} has a valid state. If valid, the
+	 * {@link PredictionVertex targetVertex} can be reached on the given
+	 * {@link Board}.
+	 * 
+	 * @param targetSpeed    speed after the edge
+	 * @param board          {@link Board} to move on
+	 * @param targetPosition {@link Point2i} position of the {@link PredictionVertex
+	 *                       targetVertex}
+	 * @param edgePoints     {@link Point2i points} of the edge
+	 * @param collisionSet   {@link Point2i points} to check for additional
+	 *                       collisions
+	 * @return true if {@link PredictionVertex targetVertex} can be reached
+	 */
+	private boolean calculateValid(final Board<Cell> board, final int targetSpeed, final Point2i targetPosition,
+			final Collection<Point2i> edgePoints, final Set<Point2i> collisionSet) {
+
+		if (targetSpeed > IPlayer.MAX_SPEED && targetSpeed < IPlayer.MIN_SPEED) {
+			return false;
+		}
+		if (!board.isOnBoard(targetPosition)) {
+			return false;
+		}
+		return hasNoCollisions(board, edgePoints, collisionSet);
+	}
+
+	/**
+	 * Checks if the {@link PredictionEdge} collides with its ancestors or with
+	 * occupied cells on the {@link Board}.
+	 * 
+	 * @param board        {@link Board} to check for collisions
+	 * @param edgePoints   {@link Point2i} points on the edge
+	 * @param collisionSet {@link Point2i} points to check for additional collision
+	 * @return true if no collision is detected
+	 */
+	private boolean hasNoCollisions(final Board<Cell> board, final Collection<Point2i> edgePoints,
+			final Set<Point2i> collisionSet) {
+
+		for (final Point2i point : edgePoints) {
+			if (!board.getBoardCellAt(point).isEmpty())
+				return false;
+			if (collisionSet.contains(point))
+				return false;
+		}
+		return true;
 	}
 
 	/**
--- spe-ed-solver/player/src/main/java/player/analysis/enemyprobability/PredictionVertex.java
+++ spe-ed-solver/player/src/main/java/player/analysis/enemyprobability/PredictionVertex.java
@@ -1,7 +1,9 @@
 package player.analysis.enemyprobability;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 
 import utility.game.board.Board;
 import utility.game.board.Cell;
@@ -25,6 +27,8 @@ public class PredictionVertex {
 
 	private final int round;
 
+	private final Set<Point2i> requiredPoints;
+
 	/**
 	 * Creates a new {@link PredictionVertex} with the given information.
 	 * 
@@ -41,6 +45,28 @@ public class PredictionVertex {
 		this.direction = direction;
 		this.speed = speed;
 		this.round = round;
+		this.requiredPoints = Collections.emptySet();
+	}
+
+	/**
+	 * Creates a new {@link PredictionVertex} with the given information.
+	 * 
+	 * @param board          {@link Board} to test for collisions with
+	 * @param position       {@link Point2i} of the {@link PredictionVertex}
+	 *                       position
+	 * @param direction      input {@link PlayerDirection} of the vertex
+	 * @param speed          input speed of the vertex
+	 * @param round          round the vertex is relevant for
+	 * @param requiredPoints {@link Point2i} points required to reach this vertex
+	 */
+	public PredictionVertex(final Board<Cell> board, final Point2i position, final PlayerDirection direction,
+			final int speed, final int round, final Set<Point2i> requiredPoints) {
+		this.board = board;
+		this.position = position;
+		this.direction = direction;
+		this.speed = speed;
+		this.round = round;
+		this.requiredPoints = Collections.emptySet();
 	}
 
 	/**
@@ -56,6 +82,7 @@ public class PredictionVertex {
 		this.direction = player.getDirection();
 		this.speed = player.getSpeed();
 		this.round = round;
+		this.requiredPoints = Collections.emptySet();
 	}
 
 	public List<PredictionEdge> getValidEdges() {
@@ -98,6 +125,13 @@ public class PredictionVertex {
 	 */
 	public int getRound() {
 		return round;
+	}
+
+	/**
+	 * @return the requiredPoints
+	 */
+	public Set<Point2i> getRequiredPoints() {
+		return requiredPoints;
 	}
 
 }
