077acd6a7c1530f0013228e5790d6d7c31653bea
Felix Lehnerer <48395059+Felixleh@users.noreply.github.com>
2021-01-05 17:36:15 +0100

    added SlowDown for preferring slower chosen actions

---
+++ spe-ed-solver/player/src/main/java/player/analysis/slowdown/SlowDown.java
@@ -0,0 +1,54 @@
+package player.analysis.slowdown;
+
+import player.analysis.ActionsRating;
+import player.analysis.PredictivePlayer;
+import utility.game.board.Board;
+import utility.game.board.Cell;
+import utility.game.player.IPlayer;
+import utility.game.player.PlayerAction;
+
+/**
+ * Class to calculate a {@link ActionsRating} which prioritizes
+ * {@link PlayerAction PlayerActions} with a lower speed.
+ */
+public class SlowDown {
+
+	/**
+	 * Calculates the {@link ActionsRating} for the given {@link Player} on the
+	 * given {@link Board}. The {@link ActionsRating} is favoring
+	 * {@link PlayerAction actions} slower speeds.
+	 * 
+	 * @param self  {@link IPlayer} to test for
+	 * @param board {@link Board} to check for collisions
+	 * @return result as {@link ActionsRating}
+	 */
+	public ActionsRating getActionsRating(final IPlayer self, final Board<Cell> board) {
+
+		final PredictivePlayer startPlayer = new PredictivePlayer(self);
+		final ActionsRating result = new ActionsRating();
+
+		for (final PlayerAction action : PlayerAction.values()) {
+			final PredictivePlayer nextPlayer = new PredictivePlayer(startPlayer, action, board);
+			final float ratingValue = ratingValue(startPlayer, nextPlayer);
+			result.setRating(action, ratingValue);
+		}
+		return result;
+	}
+
+	/**
+	 * Calculates the single rating value for two {@link IPlayer players}. One has
+	 * to be the parent, one the child.<br>
+	 * Calculation: 0 (speed up), 0.5 (keeping speed), 1 (slow down)
+	 * 
+	 * @param parent {@link IPlayer} parent
+	 * @param child  {@link IPlayer} child
+	 * @return rating value result as float
+	 */
+	private float ratingValue(final IPlayer parent, final IPlayer child) {
+		if (child.isActive())
+			return (parent.getSpeed() - child.getSpeed()) / 2f + 0.5f;
+		else
+			return 0;
+	}
+
+}
