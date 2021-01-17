921b84a4cd6372a5aaa0acb4ab8524633742ce2c
SilasDerProfi <silas@schnurr.one>
2021-01-17 10:07:44 +0100

    Add simulation tests

---
+++ spe-ed-solver/simulation/src/test/java/simulation/PlayerDeadlineTest.java
@@ -0,0 +1,26 @@
+package simulation;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+
+public class PlayerDeadlineTest {
+
+    @Test
+    public void testGetRemainingMilliseconds() throws InterruptedException {
+
+        final PlayerDeadline deadlineA = new PlayerDeadline(100);
+        final PlayerDeadline deadlineB = new PlayerDeadline(1000);
+
+        TimeUnit.MILLISECONDS.sleep(2000);
+
+        assertTrue(deadlineA.getRemainingMilliseconds() > 0);
+        assertTrue(deadlineB.getRemainingMilliseconds() > 0);
+
+        TimeUnit.MILLISECONDS.sleep(100);
+        assertTrue(deadlineA.getRemainingMilliseconds() <= 0);
+    }
+}
---
+++ spe-ed-solver/simulation/src/test/java/simulation/SimulationDeadlineTest.java
@@ -0,0 +1,42 @@
+package simulation;
+
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+
+public class SimulationDeadlineTest {
+
+    @Test
+    public void testResetDeadLine() throws InterruptedException {
+
+        final SimulationDeadline deadline = new SimulationDeadline();
+        TimeUnit.MILLISECONDS.sleep(300);
+        final long milliseconds = deadline.getRemainingMilliseconds();
+
+        deadline.resetDeadLine();
+        final long resetMilliseconds = deadline.getRemainingMilliseconds();
+        assertNotEquals(resetMilliseconds, milliseconds);
+    }
+
+    @Test
+    public void testSetTimeLimit() {
+
+        SimulationDeadline.setLowerTimeLimit(1);
+        SimulationDeadline.setUpperTimeLimit(1);
+        final SimulationDeadline oneSecondDeadline = new SimulationDeadline();
+        assertTrue("Deadline upper limit must be the maximum", oneSecondDeadline.getRemainingMilliseconds() < 1000);
+
+        SimulationDeadline.setLowerTimeLimit(5);
+        SimulationDeadline.setUpperTimeLimit(1);
+        final SimulationDeadline swappedDeadline = new SimulationDeadline();
+        assertTrue("Deadline upper limit must be the maximum", swappedDeadline.getRemainingMilliseconds() > 1000);
+        assertTrue("Deadline lower limit must be the minimum", swappedDeadline.getRemainingMilliseconds() < 5000);
+
+        // reset the limits
+        SimulationDeadline.setLowerTimeLimit(2);
+        SimulationDeadline.setUpperTimeLimit(15);
+    }
+}
